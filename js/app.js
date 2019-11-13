a9a96a2c7e64cbd12fde65c7c62565afb134553b
daniaalrababa9 <rababadania@gmail.com>
2019-11-13 14:00:15 +0200

    add

--- js/app.js
+++ js/app.js
@@ -1,61 +1,126 @@
 'use strict';
 
-function Horns(data) {
-  this.image_url = data.image_url;
-  this.title = data.title;
-  this.description = data.description;
-  this.keyword = data.keyword;
-  this.horns = data.horns;
-  Horns.all.push(this);
+//global variables
+const pageOne = './data/page-1.json';
+const pageTwo = './data/page-2.json';
+
+//global array to hold animals
+let animalArray = [];
+
+//constructor function
+function Animal(obj) {
+  for(let key in obj){
+    this[key] = obj[key];
+  }
 }
-Horns.all = [];
 
-Horns.prototype.render = function() {
-
-  // Create a new empty div tag
-  let hornOutput = $('<div></div>');
-      hornOutput.addClass(this.keyword);
-
-  // clone (copy) the html from inside the photo-template
-  let template = $('#photo-template').html();
-
-  // Add the template to the output div
-  hornOutput.html( template );
-
-  // Put the data in
-  hornOutput.find('h2').text( this.title );
-  hornOutput.find('img').attr('src', this.image_url);
-  hornOutput.find('p').text(this.description);
-
-  $('main').append(hornOutput);
-
+Animal.prototype.toHtml = function() {
+  let source = $('#photo-template').html();
+  let template = Handlebars.compile(source);
+  return template(this);
 };
 
-function populateSelectBox() {
-  let seen = {};
-  let select = $('select');
-  Horns.all.forEach( (horn) => {
-    if ( ! seen[horn.keyword] ) {
-      let option = `<option value="${horn.keyword}">${horn.keyword}</option>`;
-      select.append(option);
-      seen[horn.keyword] = true;
+Animal.prototype.toDropdown = function() {
+  let source = $('#animalList').html();
+  let template = Handlebars.compile(source);
+  console.log('dania',source)
+  return template(this);
+};
+// read json
+const readJson = (pageNumber) => {
+
+  animalArray = [];
+  $.get(pageNumber)
+    .then(animalData => {
+      animalData.forEach(animal => {
+        animalArray.push(new Animal(animal));
+      });
+    })
+    .then(titleSort);
+};
+
+//read global array activate render
+const loadAnimals = () => {
+  animalArray.forEach(animal => {
+
+    $('main').append(animal.toHtml());
+  });
+  dropDrown();
+};
+
+//function to build and display dropdown menu
+const dropDrown = () => {
+  animalArray.forEach(animal => {
+    let exists = false;
+    $('#selectBox option').each(function(){
+      if(this.value === animal.keyword){
+        exists = true;
+      }
+    });
+    if(exists === false){
+      //add element to parent
+      $('select').append(animal.toDropdown());
     }
   });
+};
 
-  console.log(seen);
-}
+//Event handler function
+let animalSelector = (event) => {
+  $('section').hide();
+  let img = $(`img[value="${event.target.value}"]`).parent();
+  $(img).show();
+};
 
-$('select').on('change', function() {
-  let selected = $(this).val();
-  $('div').hide();
-  $(`.${selected}`).fadeIn(800);
-});
+//Drop-down list event handler
+$('#selectBox').on('change', animalSelector);
 
-$.get('../data/page-1.json')
-  .then( data => {
-    data.forEach( (thing) => {
-      let horn = new Horns(thing);
-      horn.render();
+// Json page selector functions
+let pageOneSelector = () => {
+  // Animal.holdingArray = [];
+  $('section').remove();
+  readJson(pageOne);
+};
+let pageTwoSelector = () => {
+  // Animal.holdingArray = [];
+  $('section').remove();
+  readJson(pageTwo);
+};
+
+//Sort functions
+let titleSort = () => {
+  animalArray.forEach( () => {
+    animalArray.sort( (a,b) => {
+      if(a.title < b.title){
+        return -1;
+      }
+      if(a.title > b.title){
+        return 1;
+      }
+      return 0;
     });
-  })
-  .then( () => populateSelectBox() );
+    return animalArray;
+  });
+  $('section').remove();
+  loadAnimals();
+};
+
+let hornSort = () => {
+  animalArray.forEach( () => {
+    animalArray.sort( (a,b) => {
+      return a.horns - b.horns;
+    });
+    return animalArray;
+  });
+  $('section').remove();
+  loadAnimals();
+};
+
+//Button event handlers to switch pages
+$('#pageOne').on('click', pageOneSelector);
+$('#pageTwo').on('click', pageTwoSelector);
+
+//Button event handlers to sort
+$('#title').on('click', titleSort);
+$('#horns').on('click', hornSort);
+//start it off
+$(() => readJson(pageOne));
